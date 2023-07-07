b29b44e327de25b0721acf0b6cdfd54f9b7a17d6
Rahmin <rahminshoukoohi@gmail.com>
2023-07-07 01:59:59 -0400

    destructured req.body

--- App/Routes/auth/auth.js
+++ App/Routes/auth/auth.js
@@ -1,27 +1,22 @@
 // this is where all the create account, login, logout, and forgot password routes are located
 // this is where the user is redirected to the correct page after creating an account
 
+import express from 'express';
+import db from '../../../Database/models/index.js';
+import bcrypt from 'bcrypt';
+console.log(db.models.User);
 
-import express from 'express'
-import db from '../../../Database/models/index.js'
-console.log(db.models.User)
+const User = db.models.User;
 
-const User = db.models.User
+const router = express.Router();
 
+router.post('/create-account', async (req, res) => {
+	const { username, password } = req.body;
+	await User.create({
+		username: req.body.username,
+		password: req.body.password,
+	});
+	res.send('gg');
+});
 
-const router = express.Router()
-
-
-
-
-
-router.post('/create-account', async (req, res) =>{
-    await User.create({
-        username: req.body.username,
-        password: req.body.password
-    })
-    res.send('gg')
-})
-
-
-export default router
+export default router;
